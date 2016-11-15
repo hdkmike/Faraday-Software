d7c70b327d4adf9f0db4cfd7a3749bce9d0d3ab4
Brenton Salmi <brent@faradayrf.com>
2016-11-15 00:42:41 -0800

    Working simple files raw
    Working open file and transmit to RX. RX just prints.

--- Tutorials/FaradayIO/Simple_File/B_Test_Tx.py
+++ Tutorials/FaradayIO/Simple_File/B_Test_Tx.py
@@ -1,4 +1,6 @@
 import faraday_msg
+import os
+
 
 #Variables
 local_device_callsign = 'kb1lqd'  # Callsign of the local unit to connect to (COM port assignment)
@@ -15,8 +17,11 @@ faraday_tx_msg_object = faraday_msg.MessageAppTx(local_device_callsign, local_de
 # Update destination callsign (not needed but here for example)
 faraday_tx_msg_object.updatedestinationstation(remote_callsign, remote_id)
 
-# Create message to transmit
-message = '0123456789'*25
+filename = "test.txt"
+print "File size (Bytes):", os.stat(filename).st_size
+
+f = open(filename, "rb")
+message = f.read(int(os.stat(filename).st_size))
 
 # Create message fragments
 faraday_tx_msg_sm.createmsgpackets('kb1lqd', 1, message)
@@ -25,5 +30,10 @@ faraday_tx_msg_sm.createmsgpackets('kb1lqd', 1, message)
 for i in range(0, len(faraday_tx_msg_sm.list_packets), 1):
     print "TX:", repr(faraday_tx_msg_sm.list_packets[i])
     faraday_tx_msg_object.transmitframe(faraday_tx_msg_sm.list_packets[i])
+
+
+
+
+
 
 
